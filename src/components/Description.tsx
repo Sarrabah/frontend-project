2dfb9f9df78bc6f7753ed3ce95a7357787a2a65d
sarrabah <sarrabenarbia1996@gmail.com>
2025-06-20 18:30:23 +0200

    refactor: Improve description component

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import React from "react"
-import {Typography, Row, Col, Space, Button} from "antd"
+import { Typography, Row, Col, Space } from "antd"
+import { Button } from "./ui/Button"
 import "../styles/Description.css"
 
 const { Title, Paragraph } = Typography
@@ -13,22 +14,22 @@ interface DescriptionProps {
 
 const Description: React.FC<DescriptionProps> = ({ onSignInClick, onSignUpClick }) => {
   return (
-    <section className="description-section">
+    <section className={`description-section`}>
       <Row gutter={[48, 48]} align="middle">
         <Col xs={24} lg={12}>
           <div className="description-content">
             <Title level={1} className="description-title">
               Simplify Quote Management for Interior Architects
             </Title>
-            <Paragraph className="description-description">
+            <Paragraph className="description-text">
               Art Crea Pro is a platform for building and construction professionals, streamlining the management of
               quotes and quote requests between interior architects and hardware stores or craftsmen.
             </Paragraph>
-            <Space size="large">
-              <Button size="large" className="cta-btn-outline" onClick={onSignInClick}>
+            <Space size="large" className="description-actions">
+              <Button  variant="primary" size="large" onClick={onSignInClick}>
                 Sign in
               </Button>
-              <Button type="primary" size="large" className="cta-btn-primary" onClick={onSignUpClick}>
+              <Button variant="primary" size="large" onClick={onSignUpClick}>
                 Sign up
               </Button>
             </Space>
@@ -36,7 +37,18 @@ const Description: React.FC<DescriptionProps> = ({ onSignInClick, onSignUpClick 
         </Col>
         <Col xs={24} lg={12}>
           <div className="description-illustration">
-            <img src="/building.svg" alt="building logo" width="800" height="400" loading="eager"/>
+            <picture>
+              <source srcSet="/building.svg" type="image/svg" />
+              <img
+                src="/building.svg"
+                alt="Building and construction professionals platform illustration"
+                width="800"
+                height="400"
+                loading="lazy"
+                decoding="async"
+                className="description-image"
+              />
+            </picture>
           </div>
         </Col>
       </Row>
@@ -44,6 +56,4 @@ const Description: React.FC<DescriptionProps> = ({ onSignInClick, onSignUpClick 
   )
 }
 
-export default React.memo(Description);
-
-//export default Description
+export default React.memo(Description)
